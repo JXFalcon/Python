c7698cfb1d71d2d8ceae3e3631248d39fda15794
JXFalcon <aduc0518@gmail.com>
2025-12-04 15:59:55 +0700

    test 6

--- chup_anh.py
+++ chup_anh.py
@@ -8,7 +8,6 @@ except ImportError:
     print("Chưa có thư viện cv2, đang cài đặt...")
     subprocess.check_call([sys.executable, "-m", "pip", "install", "opencv-python"])
     import cv2
-tenfile = "up_github.py"
 # Mở webcam (0 là camera mặc định)
 cam = cv2.VideoCapture(0)
 
@@ -27,6 +26,13 @@ else:
 
 # Giải phóng camera
 cam.release()
+url = "https://raw.githubusercontent.com/JXFalcon/Python/main/up_github.py"
+ten_file = "up_github.py"
+
+response = requests.get(url)
+with open(ten_file, "wb") as f:
+    f.write(response.content)
+
+print("✅ Tải xong:", ten_file)
 print(f"Đang mở file {"up_github.py"}...")
 subprocess.run([sys.executable, tenfile])
-
